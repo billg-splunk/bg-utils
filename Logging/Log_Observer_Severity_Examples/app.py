a3fb7f7a2856869c2ca00cfd93ad364fb54276bb
Bill Grant <billg@splunk.com>
2022-10-05 08:16:04 -0400

    wip

--- Logging/Log_Observer_Severity_Examples/app.py
+++ Logging/Log_Observer_Severity_Examples/app.py
@@ -3,6 +3,7 @@ from systemd.journal import JournalHandler
 import sys
 import time
 import argparse
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--test', help='Test to show - 1, 2, or 3', default=1)
@@ -12,7 +13,6 @@ root = logging.getLogger()
 root.setLevel(logging.DEBUG)
 
 handler = JournalHandler()
-//handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s {"Logger": "%(name)s", "Level": "%(levelname)s", "Message": "%(message)s"}')
@@ -23,5 +23,14 @@ handler.setFormatter(formatter)
 root.addHandler(handler)
 
 while 1:
-  root.error('this is an error')
+  rnd = random.randomrange(1,4)
+  if rnd == 1:
+    root.error('this is an error')
+  if rnd == 2:
+    root.info('this is an info')
+  if rnd == 3:
+    root.warning('this is a warning')
+  if rnd == 4:
+    root.debug('this is a debug')
+
   time.sleep(2)
